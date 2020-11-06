39fa0a60d943fceb060da90a4108a2701cadf5b9
Christian <carl_hassel@hotmail.com>
2020-11-06 16:09:32 +0100

    Archive artifact test

--- src/main/java/kea/design/exam/imdb/InternetMusicDatabaseApplication.java
+++ src/main/java/kea/design/exam/imdb/InternetMusicDatabaseApplication.java
@@ -8,7 +8,7 @@ public class InternetMusicDatabaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InternetMusicDatabaseApplication.class, args);
-        System.out.println("Test");
+        System.out.println("Archive artifact test");
     }
 
 }
