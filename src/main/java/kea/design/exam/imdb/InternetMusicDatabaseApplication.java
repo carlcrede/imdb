b23b253c0632185959e8c2faf8db922c47071727
Christian <carl_hassel@hotmail.com>
2020-11-06 14:51:43 +0100

    Initial commit

---
+++ src/main/java/kea/design/exam/imdb/InternetMusicDatabaseApplication.java
@@ -0,0 +1,13 @@
+package kea.design.exam.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetMusicDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetMusicDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/design/exam/imdb/InternetMusicDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.design.exam.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetMusicDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
