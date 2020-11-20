ce86c92f9a14b1c6113d696204f95731857983a6
Christian <carl_hassel@hotmail.com>
2020-11-20 12:21:01 +0100

    Test email notification

--- src/test/java/kea/design/exam/imdb/InternetMusicDatabaseApplicationTests.java
+++ src/test/java/kea/design/exam/imdb/InternetMusicDatabaseApplicationTests.java
@@ -2,12 +2,14 @@ package kea.design.exam.imdb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class InternetMusicDatabaseApplicationTests {
 
     @Test
     void contextLoads() {
+        boolean test = false;
+        assertTrue(test);
     }
-
 }
