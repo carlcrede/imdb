c1a5ee191a25ef674440340739f74435e8e5dc1a
Deji Ehinlanwo <ehinlanwo.deji@gmail.com>
2020-11-26 11:42:51 +0100

    spotify creates token

--- src/main/java/repository/external/SpotifyRepository.java
+++ src/main/java/repository/external/SpotifyRepository.java
@@ -1,6 +1,11 @@
 package repository.external;
 
+import org.json.JSONObject;
+
+import javax.print.URIException;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,6 +14,7 @@ import java.util.Base64;
 public class SpotifyRepository {
     private String clientId;
     private String clientSecret;
+
     SpotifyRepository(String clientId, String clientSecret){
         this.clientId = clientId;
         this.clientSecret = clientSecret;
@@ -16,24 +22,28 @@ public class SpotifyRepository {
 
     public String createToken(){
         String clientAuthorization = Base64.getEncoder().encodeToString((clientId+":"+clientSecret).getBytes());
+        try {
+            //builds Http post request with httpClient
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://accounts.spotify.com/api/token"))
+                    .header("Authorization", "Basic " + clientAuthorization)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
+                    .build();
 
-        //builds Http post request with httpClient
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://accounts.spotify.com/api/token"))
-                .header("Authorization", "Basic " + clientAuthorization)
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
-                .build();
+            //sends request returning a http response with response code and body
+            HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        //sends request returning a http response with response code and body
-        HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        //checks if request returned proper http status code (200 OK)
-        //then parses json from response body and sets accesstoken to proper value (note: token needs to be refreshed after it times out)
-        if(res.statusCode() == 200) {
-            JSONObject response = new JSONObject(res.body());
-            accessToken = (String) response.get("access_token");
+            //checks if request returned proper http status code (200 OK)
+            //then parses json from response body and sets accesstoken to proper value (note: token needs to be refreshed after it times out)
+            if (res.statusCode() == 200) {
+                JSONObject response = new JSONObject(res.body());
+                 return (String) response.get("access_token");
+            }
+        }catch (URISyntaxException | IOException | InterruptedException uri){
+            uri.printStackTrace();
         }
+        return null;
     }
 }
