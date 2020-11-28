cfb86b98bb953f7958945df6ae9aced7b8e08037
Christian <carl_hassel@hotmail.com>
2020-11-28 13:10:31 +0100

    Visning af search results

--- src/main/resources/static/javascript/searchBar.js
+++ src/main/resources/static/javascript/searchBar.js
@@ -1,11 +1,13 @@
 function getSearchResults(query, type) {
+    //if (query.length < 2) return;
     $.ajax({
         url: "/search/" + type,
         data: {query: query},
         success: function (data) {
+            $("#searchResults").empty();
             $.each(data, function (index, value) {
                 console.log(value);
-                $("#searchResults").append("<li onclick='"+value.spotifyId+"'>" + value.name + "</li>");
+                $("#searchResults").append("<div onclick='"+value.spotifyId+"'>" + value.name + "</div>");
             })
         }
     })
