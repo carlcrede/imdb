03af93fefe7375f254394efc24bb74b51324e357
Deji Ehinlanwo <ehinlanwo.deji@gmail.com>
2020-11-30 12:27:31 +0100

    start forfra

--- src/main/java/kea/design/exam/imdb/repository/internal/AlbumRepository.java
+++
@@ -1,9 +0,0 @@
-package kea.design.exam.imdb.repository.internal;
-
-import kea.design.exam.imdb.models.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface AlbumRepository extends JpaRepository<Album, String> {
-}
