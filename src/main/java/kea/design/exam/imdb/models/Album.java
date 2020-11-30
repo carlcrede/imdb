504abb05508ab188f396931f4e09985d1645c2a4
ReallyTheRealCodeGod <lil.jdean@gmail.com>
2020-11-30 11:43:10 +0100

    added Album model and webjars that included bootstrap and jquery.

--- src/main/java/kea/design/exam/imdb/models/Album.java
+++ src/main/java/kea/design/exam/imdb/models/Album.java
@@ -1,16 +1,44 @@
 package kea.design.exam.imdb.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+//@SecondaryTables({
+//        @SecondaryTable(name=""),http://www.thejavageek.com/2014/09/18/jpa-secondarytable-annotation-example/
+//        @SecondaryTable(name="")
+//})
 
 @Entity
-@Table(name = "album")
+@Table(name = "release_group")
 public class Album {
 
     @Id
+    @Column(name = "id")
+    private String Id;
+
+    @Column(name = "gid")
+    private String gid;
+
+    @Column(name = "spotifyId")
     private String spotifyId;
 
+    @Column(name = "name")
     private String title;
 
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "")
+    private
+
+    @Column(name = "")
+    private
+
+    @Column(table = "", name = "")
+    private
+
+    @Column(name = "")
+    private
+
+    @Column(name = "")
+    private
 }
