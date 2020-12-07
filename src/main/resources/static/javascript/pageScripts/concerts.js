dc88c8228c1b569d47f4aef5fa2645f186d9593b
Christian <carl_hassel@hotmail.com>
2020-12-07 14:04:56 +0100

    Concerts info added - fixed artist name visibility

--- src/main/resources/static/javascript/pageScripts/concerts.js
+++ src/main/resources/static/javascript/pageScripts/concerts.js
@@ -13,10 +13,12 @@ let concerts = function setConcerts(data) {
     // TODO: add more event info
     $.each(data.events.event, function (index, value) {
         $(".concerts").append(
-            "<li class='list-group-item bg-transparent card col-sm-12'>" +
+            "<li class='list-group-item bg-transparent card col-sm-6'>" +
                 "<div class='text-light'>" +
                     "<div class='card-body'>" +
                         "<h5 class='card-title'>" + value.title + ' @ ' + value.venue_name + "</h5>" +
+                        "<div class='card-text'>" + "<strong>Where: </strong>" + value.city_name + ', ' + value.country_name + "</div>" +
+                        "<div class='card-text'>" + "<strong>When: </strong>" + $.format.date(value.start_time, 'd/MMM/yyyy HH:mm') + "</div>" +
                         "<a class='btn btn-sm btn-warning' target='_blank' href=" + value.url + ">Link to event</a> " +
                         "<a class='btn btn-sm btn-secondary' target='_blank' href=" + value.venue_url + ">Link to venue</a> " +
                     "</div>" +
