356d2c1d13324f48752d7eea2440fc054d96ff59
Christian <carl_hassel@hotmail.com>
2020-12-07 17:40:12 +0100

    Last commit today - concert endpoint builder function implemented

--- src/main/resources/static/javascript/Apis/eventfulAPI.js
+++ src/main/resources/static/javascript/Apis/eventfulAPI.js
@@ -1,7 +1,7 @@
 let key_event = "kqfXDcg7Bf7v6Lrm";
 var endpoint_event = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?";
 function getConcertsByArtist(artist, pageSize, location, sortOrder, date, callback) {
-    requestBuilder();
+    requestBuilder(artist, pageSize, location, sortOrder, date);
     $.get(endpoint_event, {}, function (data) {
         let dat = JSON.parse(data);
         callback(dat);
