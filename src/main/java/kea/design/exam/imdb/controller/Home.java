a58c04d294d14a4cf77901f2a63e5a46951b2664
Deji Ehinlanwo <ehinlanwo.deji@gmail.com>
2020-12-14 11:11:06 +0100

    lavede individuelle artist sider

--- src/main/java/kea/design/exam/imdb/controller/Home.java
+++ src/main/java/kea/design/exam/imdb/controller/Home.java
@@ -45,7 +45,6 @@ public class Home {
     public String artist(@RequestParam String id, Model model) {
         Artist artist = artistService.findByid(id);
 
-        System.out.println(artist.getBandMembers());
         model.addAttribute("artist", artist);
         model.addAttribute("album", albumService.findAlbumTypeByArtist(artist, "album"));
         return "artist";
